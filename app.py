7286f35d886508d1ef072ad15ccf2606a98afc39
Parva Shah <sparva078@gmail.com>
2025-12-27 11:33:35 -0600

    chore: Specify Flask app to run on port 5001.

--- app.py
+++ app.py
@@ -204,4 +204,4 @@ def save_chat():
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Create database tables if they don't exist.
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
